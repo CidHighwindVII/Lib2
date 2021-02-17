37b58ce3a37bf8b385aaa63a7dff0b3d22780da1
Carlos Leite <CarlosLeite@criticalmanufacturing.com>
2021-02-17 15:54:21 +0000

    initial commit

---
+++ Lib2/Lib2Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib2
+{
+    public static class Lib2Math
+    {
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
